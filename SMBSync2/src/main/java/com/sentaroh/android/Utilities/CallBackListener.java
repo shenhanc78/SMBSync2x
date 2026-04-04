bd991bca2650f0b6ad5cc2dd79791ad50a32f96e
Han Shen <shenhan@google.com>
2026-04-04 03:02:22 -0700

    refactor(deps): permanently eliminate local precompiled UI collisions and completely track natively reconstructed wrapper binaries
    Sanitizes redundant com.sentaroh.android.Utilities source stubs from local primary build hierarchies to definitively favor native global library archive classes. Additionally finalizes the pristine automated overnight generation of completely natively compiled proprietary SLF4J bytecode wrapper structures directly.

--- SMBSync2/src/main/java/com/sentaroh/android/Utilities/CallBackListener.java
+++
@@ -1,8 +0,0 @@
-package com.sentaroh.android.Utilities;
-
-import android.content.Context;
-import java.util.EventListener;
-
-public interface CallBackListener extends EventListener {
-    public boolean onCallBack(Context c, boolean positive, Object[] o);
-}
